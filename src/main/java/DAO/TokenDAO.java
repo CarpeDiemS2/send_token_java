3eafe762a078af36a88827f57a0a1835602cadc0
oliveiradeigor <lrmcpe22@gmail.com>
2022-12-09 23:05:32 -0300

    criando o tokenDAO

--- src/main/java/DAO/TokenDAO.java
+++ src/main/java/DAO/TokenDAO.java
@@ -1,2 +1,42 @@
-package DAO;public class TokenDAO {
+package DAO;
+
+import model.Token;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class TokenDAO {
+    private Connection connection;
+
+
+    public TokenDAO(Connection connection) {
+        this.connection = connection;
+    }
+
+    public List<Token> listar(){
+        List<Token> listaToken = new ArrayList<>();
+        String sql = "SELECT * FROM tbtoken";
+
+        try(PreparedStatement preparedStatement = connection.prepareStatement(sql)){
+            preparedStatement.execute();
+
+            try(ResultSet resultSet = preparedStatement.getResultSet()){
+                while (resultSet.next()){
+                    Token token = new Token(
+                            resultSet.getInt(1),
+                            resultSet.getString(2),
+                            resultSet.getInt(3)
+                    );
+                    listaToken.add(token);
+                }
+                return listaToken;
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
 }
